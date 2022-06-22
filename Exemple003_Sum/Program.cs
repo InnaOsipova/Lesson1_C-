ad607ab42c524d11491aacc3f32eaf37a564250f
Inna Osipova <rutheva_inna@mail.ru>
2022-06-22 10:25:48 +0300

    quotient

---
+++ Exemple003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int namberA = 4;
+int namberB = 7;
+int sum = namberA + namberB;
+Console.WriteLine(sum);
---
+++ Exemple004_/Program.cs
@@ -0,0 +1,7 @@
+﻿double numberA = new Random().Next(1,20);
+Console.Write("Первое сулчайное число");
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,20);
+Console.Write("Второе сулчайное число");
+Console.WriteLine(numberB);
+Console.WriteLine(numberA / numberB);
